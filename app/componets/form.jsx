8f1b7398529c70f741a017073b58820d96230153
Andrii_fsd <122934616+ribacot@users.noreply.github.com>
2024-03-07 01:08:50 +0200

    Update form.jsx

--- app/componets/form.jsx
+++ app/componets/form.jsx
@@ -5,19 +5,27 @@ import { useRef } from "react";
 export default function FormDownloadFile({ setFile, isDisabled }) {
 	const fileInput = useRef(null);
 
-	const handleFileChange = () => {
+	const handleFileChange = (e) => {
+		e.preventDefault();
 		setFile(fileInput.current.files[0]);
 	};
 
 	return (
-		<div className="flex flex-col gap-3">
-			<label className="text-center uppercase text">Download File</label>
+		<form className="flex flex-col gap-3">
+			<label className="text-center uppercase text-lg font-black">Download File</label>
 			<input
 				type="file"
-				onChange={handleFileChange}
+				// onChange={handleFileChange}
 				ref={fileInput}
 				disabled={isDisabled}
 			/>
-		</div>
+			<button
+				type="submit"
+				className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline "
+			onClick={handleFileChange}
+			>
+				Сalculation
+			</button>
+		</form>
 	);
 }
